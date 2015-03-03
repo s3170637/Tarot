30224bc9c9fae3d6bfdfa5350bf75bd61da273fd
Alexei Gudimenko <s3170637@student.rmit.edu.au>
2015-03-03 20:45:58 +1100

    Initial Commit

---
+++ Tarot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tarot
+//
+//  Created by Alexei Gudimenko on 3/03/2015.
+//  Copyright (c) 2015 Alexei Gudimenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
